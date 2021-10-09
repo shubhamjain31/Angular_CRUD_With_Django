69e9d23ceef8d28a131156d9b7c9e047e19e7bdf
shubhamjain31 <sj27754@gmail.com>
2021-10-09 23:33:10 +0530

    added dynamic html

--- frontend/src/app/restaurant/menu-restaurant/menu-add-edit-restaurant/menu-add-edit-restaurant.component.ts
+++ frontend/src/app/restaurant/menu-restaurant/menu-add-edit-restaurant/menu-add-edit-restaurant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms' 
 
 @Component({
   selector: 'app-menu-add-edit-restaurant',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-add-edit-restaurant.component.css']
 })
 export class MenuAddEditRestaurantComponent implements OnInit {
+  
+  addMenusForm: FormGroup;  
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { 
+    this.addMenusForm = this.fb.group({  
+      addMenuList: this.fb.array([]) ,  
+    });  
+  }
 
   ngOnInit(): void {
+  }
+
+
+  menu_list() : FormArray {  
+    return this.addMenusForm.get("addMenuList") as FormArray  
+  }  
+
+  newMenu(): FormGroup {  
+    return this.fb.group({  
+      name: ''  
+    })  
+  }  
+
+
+  addvalue(){
+    this.menu_list().push(this.newMenu());  
+  }
+
+  removevalue(i:number){
+    this.menu_list().removeAt(i);  
+  }
+
+  create(){
+    console.log('djs',this.addMenusForm.value )
   }
 
 }
