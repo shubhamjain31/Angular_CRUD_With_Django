58a7fae372b0b548767d7a5cde88b2c23beaa115
shubhamjain31 <sj27754@gmail.com>
2021-10-29 23:31:03 +0530

    working on display ratings

--- frontend/src/app/rating/rating.component.ts
+++ frontend/src/app/rating/rating.component.ts
@@ -4,24 +4,10 @@ import { MatTableDataSource } from '@angular/material/table';
 import { CommonService } from '../services/common.service';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  remove_col(fields: any): any;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+const ELEMENT_DATA: PeriodicElement[] = this.remove_col();
 
 @Component({
   selector: 'app-rating',
@@ -37,18 +23,27 @@ export class RatingComponent implements OnInit {
     this.commonservice.showRestaurant().subscribe((data:any) => {
       if(data['success']){
         this.restaurant_list = data['all_restaurant'];
-        console.log(this.restaurant_list)
+        console.log(this.remove_col())
       }
     });
   }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: any[] = ['fields', 'fields', 'fields'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+
+  remove_col(): any{
+    var restaurant_array = JSON.parse(JSON.stringify(this.restaurant_list));
+    for(let i in restaurant_array) {
+       delete restaurant_array[i].model;
+       delete restaurant_array[i].pk;
+    }
+    return restaurant_array;
   }
 
 }
