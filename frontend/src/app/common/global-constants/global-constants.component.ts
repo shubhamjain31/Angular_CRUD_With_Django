05fc07ebdb045ca9b20707b8c2d0ec58b412e568
shubhamjain31 <sj27754@gmail.com>
2021-11-23 23:13:40 +0530

    added stripe

--- frontend/src/app/common/global-constants/global-constants.component.ts
+++ frontend/src/app/common/global-constants/global-constants.component.ts
@@ -10,6 +10,7 @@ export class GlobalConstantsComponent implements OnInit {
   constructor() { }
 
   public static APIUrl: string = "http://127.0.0.1:8000";
+  public static STRIPE_PUBLISHABLE_KEY: string = "pk_test_51JttRQSG6SEoW7jenFmw1bOTCu1xchL283BnEPV85MZJ0i2VQ4PFuY5buV7RxbW3KnbhvhU8cK3KBKqiVC1q8Lnu00rNB6sjx1";
 
   ngOnInit(): void {
   }
--- frontend/src/app/upgrade/upgrade.component.ts
+++ frontend/src/app/upgrade/upgrade.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ GlobalConstantsComponent } from 'src/app/common/global-constants/global-constants.component';
 
 @Component({
   selector: 'app-upgrade',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upgrade.component.css']
 })
 export class UpgradeComponent implements OnInit {
+  handler:any = null;
+
+  API_KEY = GlobalConstantsComponent.STRIPE_PUBLISHABLE_KEY;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadStripe();
+  }
+
+   pay(amount: any) {    
+ 
+    var handler = (<any>window).StripeCheckout.configure({
+      key: this.API_KEY,
+      locale: 'auto',
+      token: function (token: any) {
+        console.log(token)
+        alert('Token Created!!');
+      }
+    });
+ 
+    handler.open({
+      name:         'FoodMania',
+      description: 'Delicious Food',
+      amount:       amount * 100
+    });
+ 
+  }
+ 
+  loadStripe() {
+     
+    if(!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id      = "stripe-script";
+      s.type    = "text/javascript";
+      s.src     = "https://checkout.stripe.com/checkout.js";
+
+      s.onload = () => {
+        this.handler = (<any>window).StripeCheckout.configure({
+          key: this.API_KEY,
+          locale: 'auto',
+          token: function (token: any) {
+            console.log(token)
+            alert('Payment Success!!');
+          }
+        });
+      }
+       
+      window.document.body.appendChild(s);
+    }
   }
 
 }
