def2068a2ee7a86b4e4f04fcdcc042e66498c3e3
Shubham Jain <sj27754@gmail.com>
2022-02-27 13:51:05 +0530

    update common file in angular

--- frontend/src/app/common/global-constants/global-constants.component.ts
+++ frontend/src/app/common/global-constants/global-constants.component.ts
@@ -11,10 +11,10 @@ export class GlobalConstantsComponent implements OnInit {
 
   public static APIUrl: string = "http://localhost:8000";
   public static STRIPE_PUBLISHABLE_KEY: string = "pk_test_51JttRQSG6SEoW7jenFmw1bOTCu1xchL283BnEPV85MZJ0i2VQ4PFuY5buV7RxbW3KnbhvhU8cK3KBKqiVC1q8Lnu00rNB6sjx1";
-  public static GOOGLE_API: string = "591641495893-t5otmtkmn2c5c94l1j2cv6mnivr7e4bv.apps.googleusercontent.com"
-  public static FACBOOK_API: string = "575644046874043"
+  public static GOOGLE_API: string = "YOUR_GOOGLE_API_KEY"
+  public static FACBOOK_API: string = "YOUR_FACEBOOK_API_KEY"
 
-  public static RECAPTCHA: string = '6LeDSokdAAAAAH1Q2jzDifC-dPz4Bm8gPDmxl-YV';
+  public static RECAPTCHA: string = 'YOUR_RECAPTCHA_KEY';
 
   ngOnInit(): void {
   }
